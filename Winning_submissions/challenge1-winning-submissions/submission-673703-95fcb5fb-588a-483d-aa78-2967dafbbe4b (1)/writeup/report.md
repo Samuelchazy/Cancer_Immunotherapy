dac668281d78d2a190a4293201b5712cab86d453
Samuel Chazy <samuel.chazy@gmail.com>
2023-03-20 03:21:40 -0700

    Delete Winning_submissions directory

--- Winning_submissions/challenge1-winning-submissions/submission-673703-95fcb5fb-588a-483d-aa78-2967dafbbe4b (1)/writeup/report.md
+++
@@ -1,3 +0,0 @@
-#Challenge 1:
-
-Method: Nearest Neighbors Methods, finding out the nearest 3 experiment genes and their distribution of cell state, weighted through Gaussian kernel function. 
--- Winning_submissions/challenge1-winning-submissions/submission-674490-3b339027-8f0a-471d-b603-12eadba39122 (1)/code/test.sh
+++
@@ -1,5 +0,0 @@
-python src/test.py \
-  --jsonl-path data/data_report.jsonl \
-  --model-path checkpoint/model.joblib \
-  --genes "Ets1" "Fosb" "Mafk" "Stat3" \
-  --csv-path ../solution/test_output.csv
